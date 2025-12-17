fc8f842370ad5f48aadfb9a33580a0f7a6b67a3a
Lab6th <poudel.618989@swl.k12.oh.us>
2025-12-17 06:29:28 -0500

    Done

--- src/PartOne.java
+++ src/PartOne.java
@@ -3,53 +3,86 @@ import java.util.Scanner;
 
 public class PartOne {
     public static void main(String[] args) {
-        int [] dataPoints = new int [100];
-        Random random = new Random();
-        int total = 0;
-        int average;
-        Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < dataPoints.length; i++){
-            dataPoints[i] = random.nextInt(100) + 1;
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] dataPoints = new int[100];
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
         }
 
+        System.out.println("Data Points:");
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println("\n");
+
+        int sum = 0;
         int min = dataPoints[0];
         int max = dataPoints[0];
 
-        System.out.print("These are the random numbers: \n");
-        for (int k = 0; k < dataPoints.length; k++){
-            System.out.print(dataPoints[k] +" | ");
+        for (int value : dataPoints) {
+            sum += value;
+
+            if (value < min)
+                min = value;
+
+            if (value > max)
+                max = value;
         }
 
-        //Calculation
-        for (int j = 0; j < dataPoints.length; j++){
-            total += dataPoints [j];
-            //Max value
-            if (dataPoints[j] > max) {
-                max = dataPoints[j];
-            }
-            //Min value
-            if (dataPoints[j] < min) {
-                min = dataPoints[j];
+        double average = sum / 100.0;
+
+        System.out.println("Sum of all values: " + sum);
+        System.out.println("Average value: " + average);
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+        System.out.println();
+
+        int searchValue = InputHelper.getRangedInt(
+                scan,
+                "Enter a value between 1 and 100 to count:",
+                1,
+                100
+        );
+
+        int count = 0;
+        for (int value : dataPoints) {
+            if (value == searchValue) {
+                count++;
             }
         }
-        average = total / 100;
 
-        System.out.println("\nTotal: " + total);
-        System.out.println("\nAverage: " + average);
-        System.out.println("Max value: " + max);
-        System.out.println("Min value: " + min);
+        System.out.println("The value " + searchValue + " appears " + count + " time(s) in the array.");
+        System.out.println();
 
-        //Part 2
-        int userPick = InputHelper.getRangedInt(scan,"\nPick an integer value between 1 and 100",1,100);
-        System.out.println(userPick);
-        for (int k = 0; k < dataPoints.length; k++){
-            if (dataPoints[k] == userPick) {
-                System.out.println(userPick);
+        int searchValue2 = InputHelper.getRangedInt(
+                scan,
+                "Enter another value between 1 and 100 to search for:",
+                1,
+                100
+        );
+
+        int position = -1;
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == searchValue2) {
+                position = i;
                 break;
             }
-            System.out.print(dataPoints[k] +" | ");
         }
-    }
 
+        if (position == -1) {
+            System.out.println("The value " + searchValue2 + " was NOT found in the array.");
+        } else {
+            System.out.println("The value " + searchValue2 + " was found at array index " + position + ".");
+        }
+
+        scan.close();
+    }
 }
--- src/PartTwo.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class PartTwo {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int userPick = InputHelper.getRangedInt(scan, "input an integer value between 1 and 100: ", 1,100);
-    }
-}
